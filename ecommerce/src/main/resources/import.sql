ed9b8a6f6279905a1ad89fa243150b16b5f5ea70
Diego-lz <272330@unifio.edu.br>
2026-09-15 00:40:16 +0000

    ФГ%

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into categoria (nome, descricao) values ('Livros', 'Livros Técnicos');
+
+insert into categoria (nome, descricao) values ('Eletrônicos', 'Equipamentos Eletrônicos');
+
+insert into categoria (nome, descricao) values ('Escritório', 'Material de Escritório');
+
+insert into produto (nome, descricao, estoque, preco, categoria_id) values ('Código Limpo', 'Livro do Autor Robert Martin', 20, 73.44, 1);
