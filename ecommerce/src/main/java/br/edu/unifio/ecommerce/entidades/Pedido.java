3efa18d7ce93b7c1bbd3dc2ca3baff5233f2e770
Diego-lz <272330@unifio.edu.br>
2026-09-18 01:12:10 +0000

    informar dados de pedido e item_pedido

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Pedido.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Pedido.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,6 +28,8 @@ public class Pedido {
 
     @ManyToOne
     private Cliente cliente;
+    @OneToOne 
+    private Pagamento pagamento;
 
     
 }
--- ecommerce/src/main/resources/import.sql
+++ ecommerce/src/main/resources/import.sql
@@ -1,26 +1,29 @@
--- Active: 1789684694616@@127.0.0.1@3306@ecommerce
+-- Active: 1789693090356@@127.0.0.1@3306@ecommerce
 insert into categoria (nome, descricao) values ('Livros', 'Livros Técnicos');
-
 insert into categoria (nome, descricao) values ('Eletrônicos', 'Equipamentos Eletrônicos');
-
 insert into categoria (nome, descricao) values ('Escritório', 'Material de Escritório');
-
-delete from categoria where id = 10;
 insert into categoria (nome, descricao) values ('Ginástica', 'Equipamentos de Ginástica');
 insert into categoria (nome, descricao) values ('Piscinas', 'Kit piscina 7000L');
-update categoria
-set id = 5 where id = 12;
-
 
 
 insert into produto (nome, descricao, estoque, preco, categoria_id) values ('Código Limpo', 'Livro do Autor Robert Martin', 20, 73.44, 1);
 insert into produto (nome, descricao, estoque, preco, categoria_id) values ('Fone de Ouvido Bluetooth', 'Fone de ouvido sem fio com bateria de até 40 horas, cancelamento de ruído passivo, dobrável e compatível com assistentes de voz.', 150, 249.90, 2);
 insert into produto (nome, descricao, estoque, preco, categoria_id) values ('Grampeador Metálico Jocar', 'Grampeador de mesa, capacidade para 20 folhas, corpo em metal resistente.', 60, 18.50, 3);
 insert into produto (nome, descricao, estoque, preco, categoria_id) values ('Halteres Emborrachados 5kg (Par)', 'Par de halteres revestidos em borracha, empunhadura ergonômica, ideal para treinos de força.', 45, 129.90, 4);
+insert into produto (nome, descricao, estoque, preco, categoria_id) values ('Kit Piscina 7000L Completo', 'Piscina estruturada 7000 litros com filtro, bomba, escada e capa de proteção.', 15, 899.90, 5);
+
+
 
 insert into cliente (nome, email, telefone) values ('Guilherme', 'GuiGonViado@email.com', '(14) 997341-1263');
+insert into cliente (nome, email, telefone) values ('Daniel', 'dinielCos@email.com', '(14) 992358-1273');
+insert into cliente (nome, email, telefone) values ('Gabriel', 'Biel_NXP@email.com', '(17) 994518-9873');
+insert into cliente (nome, email, telefone) values ('Felipe', 'lipeSequi@email.com', '(11) 990203-8237');
+insert into cliente (nome, email, telefone) values ('Luiz', 'Luizini@email.com', '(13) 994093-8237');
+
+
 
 insert into pedido (data, status, valor_Total, cliente_id) values ('2001-11-07', 'Enviado', 73.44, 1);
+insert into pedido (data, status, valor_Total, cliente_id) values ('2007-01-12', 'Enviado', 249.44, 2);
 
 insert into item_pedido (quantidade, valor_unitario, pedido_id, produto_id) values (15, 20.50, 1, 1);
 
